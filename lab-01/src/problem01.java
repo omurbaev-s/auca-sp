d88c7ac92bae251ae81cabcda8ed8ee865cba947
omurbaev-s <omurbaev_s@auca.kg>
2020-09-09 12:14:45 +0600

    lab-01 problem01 My "Hello, World!"

--- lab-01/src/problem01.java
+++ lab-01/src/problem01.java
@@ -1,5 +1,7 @@
 public class problem01 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("The second try?");
+        System.out.println("It seems everything is ok...");
     }
 }
