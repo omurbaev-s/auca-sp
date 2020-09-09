19bf354e552483d9e107b47d995932eca667a806
omurbaev-s <omurbaev_s@auca.kg>
2020-09-09 16:05:23 +0600

    lab-01 problem03 finished

--- lab-01/src/problem03.java
+++ lab-01/src/problem03.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class problem03 {
+    public static void main(String[] args) {
+        // Ctrl-Space
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String userName = inp.nextLine();
+        System.out.print("Hello," + userName + "!");
+
+    }
 }
