39cd418a1479ceeef0404d6b4f9b60ee41e9bc28
omurbaev-s <omurbaev_s@auca.kg>
2020-10-21 15:08:08 +0600

    lab-03 Problem08 finished

---
+++ lab-03/src/Problem08.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Problem08 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        double a = inp.nextInt();
+        double b = inp.nextInt();
+        double c = inp.nextInt();
+
+        if (c<=b && b<=a) {
+            System.out.println(c+", "+b+", "+a);
+        } else if (c<=a && a<=b) {
+            System.out.println(c+", "+a+", "+b);
+        } else if (b<=c && c<=a) {
+            System.out.println(b+", "+c+", "+a);
+        }  else if (b<=c && a<=c) {
+            System.out.println(b+", "+a+", "+c);
+        } else if (a<=b && b<=c) {
+            System.out.println(a+", "+b+", "+c);
+        } else if (a<=c && c<=b) {
+            System.out.println(a+", "+c+", "+b);
+        }
+
+    }
+}
