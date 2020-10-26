19b3a8250184b8022d94d49e51bc08d9cbe4acc7
omurbaev-s <omurbaev_s@auca.kg>
2020-10-27 03:06:47 +0600

    URI p-1041 URI 30 wrong answer 30% p-1042 accepted

--- URI_Midterm/p-1041/src/Main.java
+++ URI_Midterm/p-1041/src/Main.java
@@ -7,17 +7,17 @@ public class Main {
         double x = inp.nextDouble();
         double y = inp.nextDouble();
 
-        if (x==0 && y==0) {
+        if (x==0.0 && y==0) {
             System.out.println("Origem");
-        } else if (x==0 && y>=0) {
+        } else if (x==0.0 && y>=0) {
             System.out.println("Eixo Y");
-        } else if (y==0 && x>=0) {
+        } else if (y==0.0 && x>=0) {
             System.out.println("Eixo X");
-        } else if (x>0 && y>=0) {
+        } else if (x>0.0 && y>=0) {
             System.out.println("Q1");
-        } else if (x<=0 && y>=0) {
+        } else if (x<=0.0 && y>=0) {
             System.out.println("Q2");
-        } else if (x<=0 && y<=0) {
+        } else if (x<=0.0 && y<=0) {
             System.out.println("Q3");
         } else {
             System.out.println("Q4");
--- URI_Midterm/p-1042/src/Main.java
+++ URI_Midterm/p-1042/src/Main.java
@@ -8,6 +8,56 @@ public class Main {
         int b = inp.nextInt();
         int c = inp.nextInt();
 
+        if (a<=b && b<=c) {
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println();
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        } if (a<=c && c<=b) {
+            System.out.println(a);
+            System.out.println(c);
+            System.out.println(b);
+            System.out.println();
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        } if (b<=a && a<=c) {
+            System.out.println(b);
+            System.out.println(a);
+            System.out.println(c);
+            System.out.println();
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        } if (b<=c && c<=a) {
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(a);
+            System.out.println();
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+
+        } if (c<=a && a<=b) {
+            System.out.println(c);
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println();
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        } if (c<=b && b<=a) {
+            System.out.println(c);
+            System.out.println(b);
+            System.out.println(a);
+            System.out.println();
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        }
 
 
     }
