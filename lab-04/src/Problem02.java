e3d08457e51f9ac13667f3629ab2d4a42e95bb5e
omurbaev-s <omurbaev_s@auca.kg>
2020-11-01 19:15:03 +0600

    lab-04 Problem02 finished

---
+++ lab-04/src/Problem02.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Month? ");
+        int m = inp.nextInt();
+
+
+          if ( m==12 || m==1 || m==2){
+            System.out.println("Winter");
+        } else if (m==3 || m==4 || m==5) {
+            System.out.println("Spring");
+        } else if (m==6 || m==7 || m==8) {
+            System.out.println("Summer");
+        } else if (m==9 || m==10 || m==11) {
+            System.out.println("Autumn");
+        }
+    }
+}
