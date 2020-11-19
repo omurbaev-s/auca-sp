f550f31cd64106f53b0fd64037c25fed23f62b0c
omurbaev-s <omurbaev_s@auca.kg>
2020-11-20 00:01:35 +0600

    URI p-1047 wrong answer 5%

---
+++ URI_Midterm/p-1047/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int x1 = inp.nextInt();
+        int x2 = inp.nextInt();
+        int y1 = inp.nextInt();
+        int y2 = inp.nextInt();
+        int m;
+        int h;
+
+        if(x1==y1 && x2==y2){
+            System.out.println("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+        } if(x1==y1 && x2<y2){
+            h=0;
+            m=y2-x2;
+            System.out.println("O JOGO DUROU "+h+" HORA(S) E "+m+" MINUTO(S)");
+        } if(x1==y1 && x2>y2){
+            h=24-1;
+            m=(60-x2)+y2;
+            System.out.println("O JOGO DUROU "+h+" HORA(S) E "+m+" MINUTO(S)");
+        } if(x1<y1 && x2==y2){
+            h=y1-x1;
+            System.out.println("O JOGO DUROU "+h+" HORA(S) E 0 MINUTO(S)");
+        } if(x1>y1 && x2==y2){
+            h=(23-x1)+y1+1;
+            System.out.println("O JOGO DUROU "+h+" HORA(S) E 0 MINUTO(S)");
+        } if(x1<y1 && x2<y2){
+            h=y1-x1;
+            m=y2-x2;
+            System.out.println("O JOGO DUROU "+h+" HORA(S) E "+m+" MINUTO(S)");
+        } if(x1<y1 && x2>y2){
+            h=y1-x1-1;
+            m=(60-x2)+y2;
+            System.out.println("O JOGO DUROU "+h+" HORA(S) E "+m+" MINUTO(S)");
+        } if(x1>y1 && x2>y2){
+                h=(23-x1)+y1+1;
+                m=(60-x2)+y2;
+                System.out.println("O JOGO DUROU "+h+" HORA(S) E "+m+" MINUTO(S)");
+            } if(x1>y1 && x2<y2){
+            h=(23-x1)+y1+1;
+            m=y2-x2;
+            System.out.println("O JOGO DUROU "+h+" HORA(S) E "+m+" MINUTO(S)");
+        }
+
+    }
+}
