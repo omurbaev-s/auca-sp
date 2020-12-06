5f2884728732c386863926b255b862c77831b993
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-06 20:23:02 +0600

    URI_Final p-1117 accepted

--- URI_Final/p-1117/src/Main.java
+++ URI_Final/p-1117/src/Main.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        double x = inp.nextDouble();
+        double x;
         double scores = 0;
         double sum = 0;
         double  media = 0;
 
         while(scores!=2){
             x = inp.nextDouble();
-            if(x>=0 && x<=10){
+            if(x<0.0 || x>10.0){
+                System.out.println("nota invalida");
+            } else {
                 sum += x;
                 scores++;
-            } else if(x<0 && x>10){
-                System.out.println("nota invalida");
             }
         }
         media = sum/2;
