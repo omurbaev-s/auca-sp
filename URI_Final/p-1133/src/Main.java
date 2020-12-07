0763d3a19d14ea3e17010c778c47d6bf648b840a
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-07 16:58:30 +0600

    URI_Final p-1133 accepted

--- URI_Final/p-1133/src/Main.java
+++ URI_Final/p-1133/src/Main.java
@@ -7,13 +7,13 @@ public class Main {
         int y = inp.nextInt();
 
         if (x < y) {
-            for (int i = x; i < y; i++) {
+            for (int i = x+1; i < y; i++) {
                 if (i % 5 == 2 || i % 5 == 3) {
                     System.out.println(i);
                 }
             }
         } else if (x > y) {
-            for (int i = y; i < x; i++) {
+            for (int i = y+1; i < x; i++) {
                 if (i % 5 == 2 || i % 5 == 3) {
                     System.out.println(i);
                 }
