91114843270a74bd1d1af127684ab737572855b4
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-10 14:34:53 +0600

    URI_Final p-1154 wrong answer 15%

---
+++ URI_Final/p-1154/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int n;
+        int i = 0;
+        int sum = 0;
+        double a;
+        while((n=inp.nextInt())>=0){
+            sum+=n;
+            i++;
+        }
+        a = (double) sum/i;
+        System.out.println(a);
+    }
+}
