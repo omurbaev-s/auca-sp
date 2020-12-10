044ac798c856f127b66474fc00ec581343fbc4a8
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-10 13:30:46 +0600

    URI_Final p-1151 presentation error 5%

--- URI_Final/p-1151/src/Main.java
+++ URI_Final/p-1151/src/Main.java
@@ -10,10 +10,10 @@ public class Main {
         while(N<0 || N>46){
             N = inp.nextInt();
         }
-        System.out.print(n1+" "+n2+" ");
+        System.out.print(n1+" "+n2);
         for(int i = 2; i<N; i++){
             n3=n1+n2;
-            System.out.print(n3+" ");
+            System.out.print(" "+n3);
             n1=n2;
             n2=n3;
         }
