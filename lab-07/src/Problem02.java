b35cc1b190e86831891ceac54d2afe217eea9b07
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-16 20:32:09 +0600

    lab-07 Problem02 finished

---
+++ lab-07/src/Problem02.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        while(true) {
+            System.out.print("Year: ");
+            if (!inp.hasNextInt()) {
+                break;
+            }
+            int year = inp.nextInt();
+
+            System.out.print("Month: ");
+            if (!inp.hasNextInt()) {
+                break;
+            }
+            int month = inp.nextInt();
+
+            try {
+                System.out.println(getNumberOfDaysInMonth(year, month));
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Incorrect number of month: "+month);
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    static int [] days = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    static int getNumberOfDaysInMonth(int year, int month) {
+        if(year<1){
+            throw new IllegalArgumentException("Incorrect year: "+year);
+        }
+        int result = days[month-1];
+        if(isLeapYear(year) && month==2){
+            result++;
+        }
+        return result;
+    }
+    static boolean isLeapYear(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+}
