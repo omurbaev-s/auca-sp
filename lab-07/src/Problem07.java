7fefd380cf68a9e188c6d1ee25994759ff8f6fff
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-18 00:43:03 +0600

    lab-07 Problem07 almost finished

---
+++ lab-07/src/Problem07.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Problem07 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the integers between 1 and 100: ");
+        int[] numbers=new int[100];
+        int[] sum=new int[100];
+        for (int i=0; i< numbers.length; i++) {
+            numbers[i] = inp.nextInt();
+            if(numbers[i]==0){
+                break;
+            }
+        }
+        for (int i=0; i< numbers.length; i++){
+            int temp =numbers[i];
+            sum[temp]++;
+        }
+        for(int i=1; i< sum.length; i++){
+            if(sum[i]==1){
+                System.out.println(i+" occurs "+sum[i]+" time");
+            } else{
+                System.out.println(i+" occurs "+sum[i]+" times");
+            }
+        }
+    }
+}
