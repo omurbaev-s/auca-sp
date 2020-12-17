170ed1331f44123dbee0d00cdcbc2e38d8f14b6e
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-17 18:19:54 +0600

    lab-07 Problem04 finished

--- lab-07/src/Problem04.java
+++ lab-07/src/Problem04.java
@@ -2,7 +2,7 @@ public class Problem04 {
     public static void main(String[] args) {
      int[] deck = new int[52];
      String[] suits = {"Spades","Hearts","Diamonds","Clubs"};
-     String[] ranks = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+     String[] ranks = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 
      for(int i=0; i<deck.length; i++){
          deck[i]=i;
