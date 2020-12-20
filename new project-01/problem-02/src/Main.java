3965b0b54f13b6eaa634bca8e077fc47f99fe1f3
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-20 23:46:26 +0600

    project-01 problem-02 in process

--- new project-01/problem-02/src/Main.java
+++ new project-01/problem-02/src/Main.java
@@ -1,6 +1,11 @@
 import processing.core.*;
 
 public class Main extends PApplet {
+    final float MAX_SIZE = 150;
+    float size = 150;
+    float size1 = 30;
+    float size2 = 150;
+    float ds = 1;
 
     public void settings() {
         fullScreen();
@@ -11,9 +16,20 @@ public class Main extends PApplet {
 
     public void draw() {
         background(0);
-        textSize(60);
-        textAlign(CENTER, CENTER);
+        fill(255,0,0);
+        textSize(size);
+        textAlign(CENTER, BOTTOM);
         text("Hello,", width/2f, height/2f);
+        textSize(size1);
+        textAlign(CENTER, CENTER);
+        text("Wonderful",width/2f, height/2f);
+        if(size==MAX_SIZE || size==20){
+            ds=-ds;
+        } if(size1==MAX_SIZE || size==30){
+            ds=-ds;
+        }
+        size+=ds;
+        size1+=ds;
     }
 
     public static void main(String[] args) {
