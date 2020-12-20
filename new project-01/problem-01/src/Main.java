c5b15d9434736a7c290f21e35d80a9e1c72e3ba4
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-20 20:22:17 +0600

    project-01 problem-01 in process

--- new project-01/problem-01/src/Main.java
+++ new project-01/problem-01/src/Main.java
@@ -4,8 +4,8 @@ import processing.core.*;
 public class Main extends PApplet {
     float x,x1,x2;
     float y,y1,y2;
-    float dx;
-    float dy;
+    float dx,dx1,dx2;
+    float dy,dy1,dy2;
 
     public void settings() {
         fullScreen();
@@ -15,11 +15,15 @@ public class Main extends PApplet {
         x = width/2f;
         y = height/2f;
         x1=width/3f;
-        y1=height/3f;
+        y1=height/2f;
         x2=width/4f;
-        y2=height/4f;
+        y2=height/2f;
         dx=10;
         dy=10;
+        dx1=10;
+        dx2=10;
+        dy1=10;
+        dy2=10;
     }
 
     public void draw() {
@@ -31,28 +35,30 @@ public class Main extends PApplet {
         fill(0,0,255);
         ellipse(x2,y2,50, 50);
         x+=dx;
-        y+=dy;
-        x1+=dx;
-        y1+=dy;
-        x2+=dx;
-        y2+=dy;
+
+        x1+=dx1;
+
+        x2+=dx2;
+
 
         if(x>=width){
             dx = -dx;
             x = width-1;
-
-        } if (x<0){
+        }if(x1>=width){
+            dx1 = -dx1;
+            x1 = width-1;
+        }if(x2>=width){
+            dx2 = -dx2;
+            x2 = width-1;
+        }if (x<0) {
             dx = -dx;
             x = 0;
-
-        }if (y>=height) {
-            dy = -dy;
-            y = height-1;
-
-        } if(y<0) {
-            dy = -dy;
-            y = 0;
-
+        }if (x1<0) {
+            dx1 = -dx1;
+            x1 = 0;
+        }if (x2<0) {
+            dx2 = -dx2;
+            x2 = 0;
         }
 
     }
