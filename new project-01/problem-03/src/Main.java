9be0cf85b1b385b8ca9ac5e4a10985e9de8ef508
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-22 03:13:18 +0600

    project-01 problem-03 in process

--- new project-01/problem-03/src/Main.java
+++ new project-01/problem-03/src/Main.java
@@ -1,17 +1,22 @@
 import processing.core.*;
-
+import javax.swing.*;
 public class Main extends PApplet {
     float size = 150;
     float x;
     float x1;
     float ds;
     float ds1;
+    String firstWord, secondWord;
 
     public void settings() {
         fullScreen();
     }
 
     public void setup() {
+        String word1 = JOptionPane.showInputDialog("Enter 1st word: ");
+        firstWord=String (word1);
+        String word2 = JOptionPane.showInputDialog("Enter 2nd word: ");
+        secondWord=String.(word2);
         x=width/2f;
         x1=width/2f;
         ds=10;
@@ -23,14 +28,14 @@ public class Main extends PApplet {
         fill(255,0,0);
         textSize(size);
         textAlign(CENTER, BOTTOM);
-        text("Hello,", x, height/2.7f);
+        text(firstWord, x, height/2.7f);
         fill(0,255,0);
         textSize(size);
         textAlign(CENTER, CENTER);
-        text("World", x1, height/2f);
+        text(secondWord, x1, height/2f);
         if(x>width || x<0){
             ds=-ds;
-        } else if(x1>width || x1<0){
+        } if(x1>width || x1<0){
             ds1=-ds1;
         }
         x+=ds;
