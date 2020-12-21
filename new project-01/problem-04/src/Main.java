4bdc732d0feef1671437bdada29306a1959ed573
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-21 23:28:29 +0600

    project-01 problem-04 in process

--- new project-01/problem-04/src/Main.java
+++ new project-01/problem-04/src/Main.java
@@ -3,6 +3,8 @@ import processing.core.*;
 public class Main extends PApplet {
     float x;
     float y;
+    float dx;
+    float dy;
 
     public void settings() {
         fullScreen();
@@ -11,13 +13,27 @@ public class Main extends PApplet {
     public void setup() {
         x=width/2f;
         y=height/2f;
+        dx=10;
+        dy=0;
     }
 
     public void draw() {
         background(0, 0, 0);
         fill(255,0,0);
         circle(x, y, 100);
-        x+=10;
+        if(key==CODED){
+            if(keyCode==UP){
+                dx=0;
+                dy=-10;
+            }if(keyCode==DOWN){
+                dx=0;
+                dy=10;
+            }
+        }
+
+        x+=dx;
+        y+=dy;
+
     }
 
     public static void main(String[] args) {
