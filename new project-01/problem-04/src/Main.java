8efeee88b1789f700f1103b3a302b45c3966a64e
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-25 16:08:05 +0600

    project 1 problem04 in process

--- new project-01/problem-04/src/Main.java
+++ new project-01/problem-04/src/Main.java
@@ -6,6 +6,17 @@ public class Main extends PApplet {
     float x=350;
     float y=50;
     float size;
+    final float D1=50;
+    float x1;
+    float y1;
+    float dx;
+    float dy;
+    final int RECT_WIDTH=650;
+    final int RECT_HEIGHT=650;
+    int rectLeft;
+    int rectTop;
+    int rectRight;
+    int rectBottom;
 
     public void settings() {
         fullScreen();
@@ -20,23 +31,40 @@ public class Main extends PApplet {
             System.out.println("Error  input is "+e);
             System.exit(0);
         }
+        frameRate(10 );
+        x1=x;
+        y1=y;
+        dx=0;
+        dy=0;
 
         background(0);
-        stroke(0,0,255);
         strokeWeight(2);
-        fill(0);
-        rect(350, 50, 650, 650);
+        fill(255);
+        rect(x, y, RECT_WIDTH, RECT_HEIGHT);
     }
     public void draw() {
-        size=650/D;
-        while(x<=width-350){
-            line(x,y,x,700);
-            x+=size;
+
+        fill(255,0,0);
+        circle(x1, y1, size);
+        if(key==CODED){
+            if(keyCode==UP){
+                dx=0;
+                dy=-D1;
+            }if(keyCode==DOWN){
+                dx=0;
+                dy=D1;
+            }if(keyCode==LEFT){
+                dx=-D1;
+                dy=0;
+            }if(keyCode==RIGHT){
+                dx=D1;
+                dy=0;
+            }
         }
-        while(y<height-50){
-            line(350,y,width-365,y);
-            y+=size;
-        }
+        x1+=dx;
+        y1+=dy;
+
+
 
     }
 
