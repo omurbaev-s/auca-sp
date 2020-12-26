c32a7f62a79b9e3b7a6d33956489733b701b49c5
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-26 13:47:50 +0600

    project-01 problem-07 in process

--- new project-01/problem-07/src/Main.java
+++ new project-01/problem-07/src/Main.java
@@ -2,21 +2,25 @@ import processing.core.*;
 
 public class Main extends PApplet {
     float x,y;
+    float angle=0;
+    float dAngle=0.05f;
 
     public void settings() {
         fullScreen();
     }
 
     public void setup() {
-        x=width/2f;
-        y=height/2f;
     }
 
     public void draw() {
         background(0, 0, 0);
+        translate(width/2f,height/2f);
+        rotate(angle);
+        circle(width/6f,0,100);
+        angle+=dAngle;
         fill(255,255,0);
         noStroke();
-        circle(x,y,150);
+        circle(0,0,200);
     }
 
     public static void main(String[] args) {
