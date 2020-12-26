5708edfafb301a6b0ebbead85663109b092cecb8
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-26 11:12:47 +0600

    project-01 problem-05 in process

---
+++ new project-01/problem-05/src/Main.java
@@ -0,0 +1,51 @@
+import processing.core.*;
+
+import javax.swing.*;
+
+public class Main extends PApplet {
+
+    int N;
+    float x1;
+    float y1;
+    float blocks;
+    float boardWidth;
+    float boardHeight;
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+        background(0);
+        N =Integer.parseInt(JOptionPane.showInputDialog("Enter the board's size [4, 12]"));
+
+        try {
+            if (N < 4 || N > 12) {
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showInputDialog(null, "Invalid number" + N);
+            System.exit(1);
+        }
+
+
+        boardWidth = width / 2f;
+        boardHeight = height / 2f;
+        blocks = boardWidth / N;
+        x1 = (width - boardWidth) / 2f;
+        y1 = 50;
+
+    }
+
+    public void draw() {
+        background(255);
+        fill(0);
+        rect(x1,50,width-1,height-1);
+
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+}
