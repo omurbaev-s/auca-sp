547966622ce208b3f78ae4163528feeaac691c08
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-26 08:26:43 +0600

    project-01 problem-06 in process

--- new project-01/problem-06/src/Main.java
+++ new project-01/problem-06/src/Main.java
@@ -13,6 +13,8 @@ public class Main extends PApplet {
     int rBottom1;
     float x;
     float y;
+    String message;
+    String mouse;
 
     public void settings() {
         fullScreen();
@@ -29,10 +31,23 @@ public class Main extends PApplet {
         rTop1=(height/2)-R_HEIGHT/2;
         rRight1=rLeft+R_WIDTH;
         rBottom1=rTop+R_HEIGHT;
+        mouse = "";
     }
 
     public void draw() {
         background(0);
+        fill(255);
+        textAlign(CENTER);
+        textSize(30);
+        if (x <= rRight && x >= rLeft && y <= rBottom && y >= rTop) {
+            message = "The center of the circle is in the left rectangle";
+        } else if (x >= rLeft1 && x <= rRight1 && y <= rTop1 && y >= rBottom1) {
+            message = "The center of the circle is in the right rectangle";
+        } else {
+            message = "The center of the circle is outside of both rectangle";
+        }
+
+
         fill(0);
         stroke(255);
         rect(rLeft, rTop, R_WIDTH, R_HEIGHT);
