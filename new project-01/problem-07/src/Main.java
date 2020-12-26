53c0250c4dfd9cd1ab84d2e275de952632b69e04
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2020-12-26 14:02:54 +0600

    project-01 problem-07 in process

--- new project-01/problem-07/src/Main.java
+++ new project-01/problem-07/src/Main.java
@@ -2,10 +2,12 @@ import processing.core.*;
 
 public class Main extends PApplet {
 
-    float angle=0;
+    float angle=PI/4;
     float dAngle=0.05f;
-    float angle1=0;
+    float angle1=PI;
     float dAngle1=0.01f;
+    float angle2=PI/6;
+    float dAngle2=0.07f;
 
     public void settings() {
         fullScreen();
@@ -17,13 +19,26 @@ public class Main extends PApplet {
     public void draw() {
         background(0, 0, 0);
         translate(width/2f,height/2f);
+        fill(255,255,0);
         circle(0,0,200);
+
+        pushMatrix();
         rotate(angle);
+        fill(0,0,255);
         circle(width/6f,0,100);
-        angle+=dAngle;
 
         translate(width/6f,0);
+        rotate(angle1);
+        fill(255);
         circle(width/20f,0,20);
+        popMatrix();
+
+        rotate(angle2);
+        circle(width/12f,0,100);
+
+        angle+=dAngle;
+        angle1+=dAngle1;
+        angle2+=dAngle2;
     }
 
     public static void main(String[] args) {
